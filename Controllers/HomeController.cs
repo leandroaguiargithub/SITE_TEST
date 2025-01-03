b730821466bafa2270570e6ddad05761fdf2d593
Aguiar <aguiarcomputing@outlook.com>
2025-01-03 19:42:47 -0300

    aguiar

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using siteAguiarComputing.Models;
+using System.Diagnostics;
+
+namespace siteAguiarComputing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Agendamento(string name, string email, string phone, string service, string date, string message)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
