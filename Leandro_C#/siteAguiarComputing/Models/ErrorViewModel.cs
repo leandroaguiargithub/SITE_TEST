7c06e500e1782f75c969e87adf7dbb8c7fa8bb57
Aguiar <aguiarcomputing@outlook.com>
2025-01-03 19:46:20 -0300

    aguiar

--- Leandro_C#/siteAguiarComputing/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace siteAguiarComputing.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
